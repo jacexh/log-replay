c37ffdc7f060d30e9684e5a7a339a4d6a50ccd95
徐豪 <xuhao@wosai-inc.com>
2016-07-09 09:30:22 +0800

    定义事件钩子

---
+++ logreplay/events.py
@@ -0,0 +1,24 @@
+__all__ = ["response", "repeat", "replay"]
+
+
+class EventHook:
+
+    def __init__(self):
+        self._handlers = []
+
+    def __iadd__(self, other):
+        self._handlers.append(other)
+        return self
+
+    def __isub__(self, other):
+        self._handlers.remove(other)
+        return self
+
+    def fire(self, **kwargs):
+        for handler in self._handlers:
+            handler(**kwargs)
+
+
+repeat = EventHook()
+replay = EventHook()
+response = EventHook()
